331d90bf5ffc46a10055e1990eada815a547040a
Abrar Hussain <abrarhussain640@gmail.com>
2026-02-23 15:26:31 +0500

    Login Page Complete

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def dashboard():
+def Wellcome():
     return render_template("Wellcome.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
