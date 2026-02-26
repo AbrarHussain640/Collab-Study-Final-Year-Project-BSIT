dba5e13d1a1698ba89d12d4b649e7a7c2dc40956
Abrar Hussain <abrarhussain640@gmail.com>
2026-02-26 16:00:29 +0500

    Register form complete and create admin dashboard page.

--- app.py
+++ app.py
@@ -12,5 +12,14 @@ def Wellcome():
 def login():
     return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
